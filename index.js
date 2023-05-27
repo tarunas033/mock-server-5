a3929f1f4121c0610b944b51389d6448f961f680
Taruna Sharma <tannis033.com>
2023-05-27 13:45:47 +0530

    add

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const express = require('express');
+const jsonServer = require('json-server');
+const cors = require('cors');
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+const app = express();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+app.use(cors()); 
+app.use(middlewares);
+app.use(router);
+app.listen(3001, () => {
+  console.log('JSON Server is running');
+});
